6571660e7224d4d30508b969fdf99a4d01853415
Victor E B Barros <99879937+victoremanuel01@users.noreply.github.com>
2023-04-21 18:50:43 -0300

    commit

--- Assets/UI/Scripts/GameController.cs
+++ Assets/UI/Scripts/GameController.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
-{
-    
+{   
+    public TextMeshProUGUI touchContinue;
+    public void SkipToTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void Update()
+    {
+        touchContinue.enabled = Mathf.PingPong(Time.time, 0.5f) > 0.25f;
+
+        
+    }
+
+
+
 }
